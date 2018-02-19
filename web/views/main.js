2b3d2bada567c9efd7c3f0238f8dbe252f8f7cb6
Christian Fei <crifei93@gmail.com>
2018-02-19 01:45:28 +0100

    add horizontal support

--- web/views/main.js
+++ web/views/main.js
@@ -1,33 +1,56 @@
 var html = require('choo/html')
 var rawHtml = require('choo/html/raw')
+var css = require('sheetify')
 var snarkdown = require('snarkdown')
 
 var TITLE = 'web - main'
+
+const horizonalPrefix = css`
+:host {
+  display: flex;
+  flex-direction: row;
+}
+:host > * {
+  min-width: 100vw;
+}
+`
+const actionPrefix = css`
+:host {
+  border-bottom: 2px solid lightgrey;
+  max-width: 40em;
+  overflow: scroll;
+}
+:host pre {
+  white-space: pre-line;
+}
+`
 
 module.exports = view
 
 function view (state, emit) {
   if (state.title !== TITLE) emit(state.events.DOMTITLECHANGE, TITLE)
 
+  let horizontal = false
+  if (typeof window !== 'undefined') {
+    horizontal = window.location.href.includes('horizontal')
+  }
+
   return html`
     <body class="code lh-copy">
-      <main class="flex">
-        <section class="w-100">
-          ${rawHtml(state.actions.map(a => description(a)).join(''))}
-        </section>
+      <main class="flexxxx ${horizontal ? horizonalPrefix : ''}">
+        ${rawHtml(state.actions.map(toDescription).join(''))}
       </main>
     </body>
   `
 }
 
-function description (action) {
+function toDescription (action, index) {
   let description = `<b>${action.date}</b> - <i>${action.memberCreator.initials}</i>`
   if (action.type === 'updateCard') {
-    description += ` moved "${action.data.card.name}" from ${action.data.listBefore.name} to ${action.data.listAfter.name}`
+    description += ` <b>moved</b> "${action.data.card.name}" from ${action.data.listBefore.name} to ${action.data.listAfter.name}`
   }
   if (action.type === 'commentCard') {
-    const md = snarkdown(action.data.text)
-    description += ` commented on card "${action.data.card.name}"\n${md}`
+    description += ` <b>commented</b> on card "${action.data.card.name}"\n${snarkdown(action.data.text)}`
   }
-  return `<div class="pa3 w-100">${description}</div>`
+  return `<section class="${actionPrefix} pa3">${description}</section>`
 }
