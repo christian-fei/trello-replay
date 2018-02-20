42d8fd3b054d6cdb7fe348c183d0a3719fd0f93e
Christian Fei <crifei93@gmail.com>
2018-02-20 23:17:19 +0100

    extract updateCard

--- web/views/main.js
+++ web/views/main.js
@@ -65,50 +65,67 @@ function view (state, emit) {
   return html`
     <body class="code lh-copy" style="font-size: 14px">
       <main class="${horizontal ? horizonalPrefix : ''}">
-        ${rawHtml(state.actions.map(toDescription).join(''))}
+        ${state.actions
+          .map(toAction)
+          .map(rawHtml)}
       </main>
     </body>
   `
 }
 
-function toDescription (action, index) {
+function toAction (action, index) {
   let description = ''
 
   const actionColour = stringToColour(action.data.card.name)
   if (action.type === 'updateCard') {
-    description += `
-      <br>
-      <h2 class="${actionCardNamePrefix}" style="background: ${actionColour}; ">
-        <img class="${cardActionPrefix}" src="/assets/forward-white.png">
-        <span>"${action.data.card.name}"</span>
-      </h2>`
-
-    description += `<h2>${action.date}</h2>
-    <br>
-    <i><img class="${avatarPrefix}" src="https://trello-avatars.s3.amazonaws.com/${action.memberCreator.avatarHash}/170.png"/></i>`
-
-    description += `
-      <br>
-      from <h1 class="di">${action.data.listBefore.name}</h1>
-      to <h1 class="di">${action.data.listAfter.name}</h1>`
+    description += toUpdateAction(action, actionColour)
   }
   if (action.type === 'commentCard') {
-    description += `
-      <br>
-      <h2 class="${actionCardNamePrefix}" style="background: ${actionColour}; ">
-          <img class="${cardActionPrefix}" src="/assets/comment-white.png">
-        <span>"${action.data.card.name}"</span>
-      </h2>`
-    description += `<h2>${action.date}</h2>
-      <br>
-      <i><img class="${avatarPrefix}" src="https://trello-avatars.s3.amazonaws.com/${action.memberCreator.avatarHash}/170.png"/></i>`
-
-    description += `
-      <br>
-      <h3>${snarkdown(action.data.text)}</h3>`
+    description += toCommentAction(action, actionColour)
   }
   return `
     <section class="${actionPrefix} pa5">${description}</section>`
+}
+
+function toUpdateAction (action, actionColour) {
+  let description = ''
+  description += `
+  <br>
+  <h2 class="${actionCardNamePrefix}" style="background: ${actionColour}; ">
+    <img class="${cardActionPrefix}" src="/assets/forward-white.png">
+    <span>"${action.data.card.name}"</span>
+  </h2>`
+
+  description += `<h2>${action.date}</h2>
+  <br>
+  <i><img class="${avatarPrefix}" src="https://trello-avatars.s3.amazonaws.com/${action.memberCreator.avatarHash}/170.png"/></i>`
+
+  description += `
+  <br>
+  from <h1 class="di">${action.data.listBefore.name}</h1>
+  to <h1 class="di">${action.data.listAfter.name}</h1>`
+
+  return description
+}
+
+function toCommentAction (action, actionColour) {
+  let description = ''
+  description += `
+  <br>
+  <h2 class="${actionCardNamePrefix}" style="background: ${actionColour}; ">
+      <img class="${cardActionPrefix}" src="/assets/comment-white.png">
+    <span>"${action.data.card.name}"</span>
+  </h2>`
+
+  description += `<h2>${action.date}</h2>
+  <br>
+  <i><img class="${avatarPrefix}" src="https://trello-avatars.s3.amazonaws.com/${action.memberCreator.avatarHash}/170.png"/></i>`
+
+  description += `
+  <br>
+  <h3>${snarkdown(action.data.text)}</h3>`
+
+  return description
 }
 
 function stringToColour (str) {
