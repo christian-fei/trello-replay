de7a81323577489fac6bc6ba271e61e7a3d7f3b5
Christian Fei <crifei93@gmail.com>
2018-02-20 21:25:54 +0100

    add some colour

--- web/views/main.js
+++ web/views/main.js
@@ -54,12 +54,14 @@ function toDescription (action, index) {
   let description = `<h2>${action.date}</h2>
     <br>
     <i>${action.memberCreator.initials}</i>`
+
+  const actionColour = stringToColour(action.data.card.name)
   if (action.type === 'updateCard') {
     description += `
       <img src="/assets/forward.png">
       moved
       <br>
-      <h2>"${action.data.card.name}"</h2>
+      <h2 style="border-bottom: 1em solid ${actionColour}; ">"${action.data.card.name}"</h2>
       <br>
       from <h1 class="di">${action.data.listBefore.name}</h1>
       to <h1 class="di">${action.data.listAfter.name}</h1>`
@@ -69,10 +71,24 @@ function toDescription (action, index) {
       <img src="/assets/comment.png">
       commented on card
       <br>
-      <h2>"${action.data.card.name}"</h2>
+      <h2 style="border-bottom: 1em solid ${actionColour}; ">"${action.data.card.name}"</h2>
       <br>
       <h3>${snarkdown(action.data.text)}</h3>`
   }
   return `
     <section class="${actionPrefix} pa5">${description}</section>`
+}
+
+
+function stringToColour (str) {
+  var hash = 0;
+  for (var i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  var colour = '#';
+  for (var i = 0; i < 3; i++) {
+    var value = (hash >> (i * 8)) & 0xFF;
+    colour += ('00' + value.toString(16)).substr(-2);
+  }
+  return colour;
 }
